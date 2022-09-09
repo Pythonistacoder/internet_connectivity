1c7530cab4d52d2fb8eac20db47328cb5016389b
Kunal Dey <pythonistacoder@gmail.com>
2022-09-09 12:24:47 +0530

    initialised the package

---
+++ lib/fixpals_internet_connectivity.dart
@@ -0,0 +1,34 @@
+library fixpals_internet_connectivity;
+
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/internet_bloc/internet_events.dart';
+import 'blocs/internet_bloc/internet_states.dart';
+
+class InternetBloc extends Bloc<InternetEvent, InternetState> {
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription? connectivitySubscription;
+  InternetBloc() : super(InternetInitialState()) {
+    on<InternetLostEvent>((event, emit) => emit(InternetLostState()));
+    on<InternetRetrievedEvent>((event, emit) => emit(InternetRetrievedState()));
+
+    connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        add(InternetRetrievedEvent());
+      } else {
+        add(InternetLostEvent());
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    connectivitySubscription?.cancel();
+    return super.close();
+  }
+}
